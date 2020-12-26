61979edfd7f0f2ccb385eaad1ce47febf10a4141
Hp <Hp@DESKTOP-HG61R7Q>
2020-12-26 18:28:34 +0530

    Accessing bean value from expression element and by jstl tag

---
+++ JSTL/src/org/studyeasy/bean/User.java
@@ -0,0 +1,28 @@
+package org.studyeasy.bean;
+
+public class User {
+	private String firstName;
+	private String lastName;
+	
+	public User() {
+		firstName = "Ashish";
+		lastName = "Madaan";
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
