d5e491e99211ff9c92b98f0144dc8f6b0b3f22d8
Hp <Hp@DESKTOP-HG61R7Q>
2020-12-30 23:23:58 +0530

    Controller for handling input images

--- FileUpload/src/org/studyeasy/ImageUpload.java
+++ FileUpload/src/org/studyeasy/ImageUpload.java
@@ -1,11 +1,18 @@
 package org.studyeasy;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * Servlet implementation class ImageUpload
@@ -14,11 +21,23 @@ import javax.servlet.http.HttpServletResponse;
 public class ImageUpload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//ServletFileUpload upload = new ServletFileUpload();
+		ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
+		
+		try {
+			List<FileItem> images = upload.parseRequest(request);
+			for(FileItem image: images) {
+				String name = image.getName();
+				name = name.substring(name.lastIndexOf("\\")+1);
+				System.out.println(name);
+			}
+		} catch (FileUploadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
